82c5d7272d8500a7e22ab45d8e3050448ebafcc2
arkadysmith <nw2025@protonmail.com>
2026-03-29 18:07:11 +0000

    Update 0-zig.sh

--- 0-zig.sh
+++ 0-zig.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo 'deb https://dl.bintray.com/dryzig/zig-debian bullseye main' | sudo tee -a /etc/apt/sources.list &&
+curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpg &&
+echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list &&
 sudo apt update &&
 sudo apt-get install -y zig
