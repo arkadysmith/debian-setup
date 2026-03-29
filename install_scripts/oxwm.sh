6f9b424a4e4e50faa1cb0e9aace8a0adcc47b593
arkadysmith <nw2025@protonmail.com>
2026-03-29 16:25:50 +0000

    Update oxwm.sh

--- install_scripts/oxwm.sh
+++ install_scripts/oxwm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/tonybanters/oxwm &&
+cd ~/oxwm &&
 sudo apt-get install -y liblua5.4-dev libx11-dev libxinerama-dev libxft-dev libfontconfig-dev &&
 sudo zig build -Doptimize=ReleaseSmall --prefix /usr &&
 oxwm --init
